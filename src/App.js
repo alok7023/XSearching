9e2d949a3e244a457b8dbd730edca4776b7bb79b
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-03-21 23:29:06 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const data = [
+  {
+    name: "Alok",
+    age: 25,
+  },
+  {
+    name: "Ravi",
+    age: 26,
+  },
+  {
+    name: "Ankit",
+    age: 24,
+  },
+  {
+    name: "Monu",
+    age: 22,
+  },
+];
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchData, setSearchData] = useState(data);
+
+  const fetchData = (query) => {
+    const result = data.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase())
+  });
+    console.log(result);
+    setSearchData(result);
+  };
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+    fetchData(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Implement Searching</h1>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+      </div>
+      <div className="list">
+        <ol>
+          {searchData.map((item) => <li>{item.name}</li>)}
+        </ol>
+      </div>
     </div>
   );
 }
